f5bdf6822eebc16aeb48afc2615483ea6216051c
David Francisco <davidfco.pozo@hotmail.com>
2022-10-20 17:17:19 -0400

    Added form to contact page

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,7 +1,17 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
+import FormText from "../components/FormText";
+import contactStyles from "../styles/pages/Contact.module.scss";
 
 const contacto = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className={contactStyles.contact}>
+        <FormText />
+        <ContactForm />
+      </div>
+    </>
+  );
 };
 
 export default contacto;
