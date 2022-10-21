d24fd4c501217f0a32f24c4ebdbc13c5fc409810
David Francisco <davidfco.pozo@hotmail.com>
2022-10-21 17:18:34 -0400

    Added extra fetch function for contact form

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useModal = () => {
+const useFetch = () => {
   const [data, setData] = useState("");
   const [error, setError] = useState("");
 
@@ -35,7 +35,34 @@ const useModal = () => {
       });
   };
 
-  return [data, error, submitData];
+  const formSubmitData = (values, onSubmitProps) => {
+    fetch("https://formsubmit.co/ajax/indidseo@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        Nombre: values.nombre,
+        Telefono: values.numTel,
+        Correo: values.correo,
+        Detalles: values.detalles,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        onSubmitProps.setSubmitting(false);
+        onSubmitProps.resetForm();
+        setData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      });
+  };
+
+  return [data, error, submitData, formSubmitData];
 };
 
-export default useModal;
+export default useFetch;
