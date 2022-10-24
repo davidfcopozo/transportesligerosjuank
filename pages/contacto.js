b5d5f583def5531f5bca9cef27a90e3a9a2a4b7a
David Francisco <davidfco.pozo@hotmail.com>
2022-10-24 10:03:05 -0400

    Nosotros page content styles added

--- pages/contacto.js
+++ pages/contacto.js
@@ -2,13 +2,15 @@ import React from "react";
 import ContactForm from "../components/ContactForm";
 import FormText from "../components/FormText";
 import contactStyles from "../styles/pages/Contact.module.scss";
+import useFormSubmit from "../hooks/useFormSubmit";
 
 const contacto = () => {
+  const [formSubmitData] = useFormSubmit();
   return (
     <>
       <div className={contactStyles.contact}>
         <FormText />
-        <ContactForm />
+        <ContactForm formSubmitData={formSubmitData} />
       </div>
     </>
   );
