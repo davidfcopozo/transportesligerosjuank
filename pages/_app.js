54f2ebbf1039db68cd772f4b0d9316111f9bdc15
David Francisco <davidfco.pozo@hotmail.com>
2022-10-25 17:04:39 -0400

    Working on page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,40 @@ import Layout from "../components/Layout";
 import "../styles/globals.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { AnimatePresence, motion } from "framer-motion";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
+  const pageTransition = {
+    pageInitial: {
+      opacity: 0,
+      x: 100,
+    },
+    pageAnimate: {
+      opacity: 1,
+      x: 0,
+      duration: 3,
+    },
+    pageExit: {
+      opacity: 0,
+      x: 100,
+      duration: 3,
+    },
+  };
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AnimatePresence>
+      <motion.div
+        variants={pageTransition}
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
