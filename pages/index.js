ffc0aeb96f6adb26e60c95a58a24ada465006e21
David Francisco <davidfco.pozo@hotmail.com>
2022-10-30 10:33:55 -0400

    Adding transition

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,56 @@ import ContactHeroImg from "../components/ContactHeroImg";
 import Services from "../components/Services";
 import heroImg from "../assets/heroImg.jpg";
 import contactHeroImg from "../assets/chico-furgoneta.jpg";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  return (
-    <div>
-      <HeroImg
-        img={heroImg.src}
-        imgAlt="Caja siendo embalada"
-        title="Mudanzas en Zaragoza"
-        btnValue="Contáctanos"
-      />
-      <Services />
+  const boxVariants = {
+    out: {
+      y: 600,
+    },
+    in: {
+      y: 0,
+      transition: {
+        duration: 0.9,
+        // The first child will appear AFTER the parrent has appeared on the screen
+        delayChildren: 0.3,
+        // The next sibling will appear 0.5s after the previous one
+        staggerChildren: 0.5,
+        stiffness: 2,
+        damping: 1,
+      },
+    },
+  };
 
-      <ContactHeroImg
-        img={contactHeroImg.src}
-        imgAlt="Chico dando servicio de mudanza"
-        title="¿Necesitas ayuda con tu mudanza?"
-        btnValue="Contáctanos"
-      />
-    </div>
+  const iconVariants = {
+    out: {
+      y: 500,
+    },
+    in: {
+      y: 0,
+    },
+  };
+  return (
+    <motion.div variants={boxVariants} initial="out" animate="in">
+      <motion.div variants={iconVariants}>
+        <HeroImg
+          img={heroImg.src}
+          imgAlt="Caja siendo embalada"
+          title="Mudanzas en Zaragoza"
+          btnValue="Contáctanos"
+        />
+      </motion.div>
+      <motion.div variants={iconVariants}>
+        <Services />
+      </motion.div>
+      <motion.div variants={iconVariants}>
+        <ContactHeroImg
+          img={contactHeroImg.src}
+          imgAlt="Chico dando servicio de mudanza"
+          title="¿Necesitas ayuda con tu mudanza?"
+          btnValue="Contáctanos"
+        />
+      </motion.div>
+    </motion.div>
   );
 }
