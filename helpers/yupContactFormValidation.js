7d00b03400e4f58f13f5105bcc932cf51f2af4a3
David Francisco <davidfco.pozo@hotmail.com>
2022-10-30 10:32:45 -0400

    Fixed validation schema

--- helpers/yupContactFormValidation.js
+++ helpers/yupContactFormValidation.js
@@ -1,28 +1,29 @@
 import * as Yup from "yup";
 
 const yupContactFormValidation = () => {
-  const initialValues = {
-    correo: "",
-    detalles: "",
-    nombre: "",
-    numTel: "",
+  const contactFormInitialValues = {
+    name: "",
+    cel: "",
+    email: "",
+    details: "",
   };
 
-  const validationSchema = Yup.object({
-    correo: Yup.string().email("Formato del correo electrónico es inválido"),
-    /* .required("Campo requerido") */
-    detalles: Yup.string() /* .required("Por favor, escriba su mensaje.") */,
-    nombre: Yup.string() /* .required("Campo requerido") */,
-    numTel: Yup.number()
+  const contactFormValidationSchema = Yup.object({
+    name: Yup.string().required("Campo requerido"),
+    cel: Yup.number()
       .typeError("Este campo solo acepta números.")
       .min(9, "Número de teléfono imcompleto")
-      .integer(),
-    /* .required("Campo requerido") */
+      .integer()
+      .required("Campo requerido"),
+    email: Yup.string()
+      .email("Formato del correo electrónico es inválido")
+      .required("Campo requerido"),
+    details: Yup.string().required("Por favor, escriba su mensaje."),
   });
 
   return {
-    validationSchema,
-    initialValues,
+    contactFormValidationSchema,
+    contactFormInitialValues,
   };
 };
 
