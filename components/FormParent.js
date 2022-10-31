21338f18c10f12bf22afd20842283ff0c8306ffa
David Francisco <davidfco.pozo@hotmail.com>
2022-10-31 14:00:49 -0400

    Submit function added and passed as prop to contact form

--- components/FormParent.js
+++ components/FormParent.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactForm from "./ContactForm";
+import { useSuccessContext } from "./context/FormSuccessContext";
 import FormText from "./FormText";
 
 const FormParent = () => {
+  const [data, setData] = useState("");
+  const [error, setError] = useState("");
+
   const styles = {
     marginInline: "auto",
     width: "80%",
@@ -10,12 +14,42 @@ const FormParent = () => {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: "15vh",
   };
+
+  const { setSuccess, setFail } = useSuccessContext();
+
+  const submitForm = (values, onSubmitProps) => {
+    fetch("https://formsubmit.co/ajax/indidseo@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        Nombre: values.name,
+        Telefono: values.cel,
+        Correo: values.email,
+        Detalles: values.details,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        onSubmitProps.setSubmitting(false);
+        onSubmitProps.resetForm();
+        setData(data);
+        setSuccess(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+        setFail(error);
+      });
+  };
+
   return (
     <div style={styles}>
       <FormText />
-      <ContactForm />
+      <ContactForm submitForm={submitForm} data={data} error={error} />
     </div>
   );
 };
