79e75d080b5524da162952ca5a30c577bd6ba01c
David Francisco <davidfco.pozo@hotmail.com>
2022-10-31 14:04:24 -0400

    Styles removed

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,16 +1,8 @@
-/* import ContactForm from "../components/ContactForm";
-import FormText from "../components/FormText";
-import contactStyles from "../styles/pages/Contact.module.scss"; */
-
 import FormParent from "../components/FormParent";
 
 const contacto = () => {
   return (
     <>
-      {/* <div className={contactStyles.contact}>
-        <FormText />
-        <ContactForm />
-      </div> */}
       <FormParent />
     </>
   );
