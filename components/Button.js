8c096298ae5d97fa4723eb186afb3fdb2081d932
David Francisco <davidfco.pozo@hotmail.com>
2022-10-31 14:21:29 -0400

    forwardRef added to button component

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { motion } from "framer-motion";
 import btnStyles from "../styles/components/Button.module.scss";
 
-const Button = ({ ...props }) => {
+const Button = React.forwardRef(({ onClick, href, ...props }, ref) => {
   return (
     <motion.button
+      ref={ref}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
       className={btnStyles.btn}
-      onClick={props.clickHandler}
+      onClick={onClick}
     >
       {props.value}
     </motion.button>
   );
-};
+});
 
 export default Button;
