80ecdcafede7c28ceffcbc840546f3887b64772f
David Francisco <davidfco.pozo@hotmail.com>
2022-11-03 13:26:21 -0400

    Name changed

---
+++ pages/terminos.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Meta from "../components/Meta";
+
+const terminos = () => {
+  return (
+    <>
+      <Meta
+        title="Terminos y Condiciones"
+        description="Terminos y Condiciones"
+      />
+      <div>terminos-y-condiciones</div>
+    </>
+  );
+};
+
+export default terminos;
