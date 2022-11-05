842d6d6466a0582cc056a2f1d303c0610ec9b9b0
David Francisco <davidfco.pozo@hotmail.com>
2022-11-05 09:44:27 -0400

    Global style import changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { AnimatePresence, motion } from "framer-motion";
