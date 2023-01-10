e4e8b9bcb845f1c806951804ff0909eedf6b5d2f
David Francisco <davidfco.pozo@gmail.com>
2023-01-10 11:34:27 -0400

    Google analytics tag added
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- components/Meta.js
+++ components/Meta.js
@@ -4,6 +4,16 @@ import React from "react";
 const Meta = ({ title, keywords, description }) => {
   return (
     <Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-5BE5PRX017"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-5BE5PRX017');
+      </script>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
