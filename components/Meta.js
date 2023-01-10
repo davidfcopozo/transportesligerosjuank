cb6435128b8121ca44dc7a4c704acba3c74b6b91
David Francisco <davidfco.pozo@gmail.com>
2023-01-10 12:28:21 -0400

    Google analytics tag added with next script to _app
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- components/Meta.js
+++ components/Meta.js
@@ -5,18 +5,6 @@ import React from "react";
 const Meta = ({ title, keywords, description }) => {
   return (
     <Head>
-      {/* <!-- Google tag (gtag.js) --> */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-5BE5PRX017"
-        strategy="afterInteractive"
-      ></Script>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-5BE5PRX017'); `}
-      </Script>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,18 @@ function MyApp({ Component, pageProps, router }) {
         exit="pageExit"
       >
         <Layout>
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-5BE5PRX017"
+            strategy="afterInteractive"
+          ></Script>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-5BE5PRX017'); `}
+          </Script>
           <Component {...pageProps} />
         </Layout>
       </motion.div>
